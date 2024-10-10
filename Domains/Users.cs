85a4adc9a4fc6b42d4cc5ea794617bf330daff64
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-10 17:53:19 +0700

    refactor: delete whitespace for each fields to same line

--- Domains/Users.cs
+++ Domains/Users.cs
@@ -2,70 +2,43 @@ namespace server.Domains
 {
   public class User
   {
-    public string Id
-    {
-      get;
-      set;
-    } = string.Empty;
-    public string Username
-    {
-      get;
-      set;
-    } = string.Empty;
-    public string Password
-    {
-      get;
-      set;
-    } = string.Empty;
-    public string Firstname
-    {
-      get;
-      set;
-    } = string.Empty;
-    public string Lastname
-    {
-      get;
-      set;
-    } = string.Empty;
-    public DateTime CreatedAt
-    {
-      get;
-      set;
-    }
-    public DateTime UpdatedAt
-    {
-      get;
-      set;
-    }
+    public string Id { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+  }
+
+  public interface IUserRepository
+  {
+    Task<User> GetUserById(string id);
+    Task<User> GetUserByUsername(string username);
+    Task CreateUser(User user);
+    Task UpdateUserInfo(UpdateUser user);
+  }
+  public interface IUserUsecases
+  {
+    Task<User> GetUserByUsername(string username);
+    Task<User> GetUserById(string id);
+    Task SignUpUser(SignUpUser user);
+    Task UpdateUser(UpdateUser user);
   }
 
   public class SignUpUser
   {
-    public string Username
-    {
-      get;
-      set;
-    } = string.Empty;
-    public string Password
-    {
-      get;
-      set;
-    } = string.Empty;
-    public string ConfirmPassword
-    {
-      get;
-      set;
-    } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+  }
 
-    public string Firstname
-    {
-      get;
-      set;
-    } = string.Empty;
-    public string Lastname
-    {
-      get;
-      set;
-    } = string.Empty;
+  public class UpdateUser
+  {
+    public string Id { get; set; } = string.Empty;
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
   }
 }
