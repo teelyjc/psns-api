4bd53e899cadf875e17f55f84ab1df098cd10470
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-10 17:55:25 +0700

    refactor: make fields capitalize and remove unused using

--- Utils/Errors/GlobalErrorFilter.cs
+++ Utils/Errors/GlobalErrorFilter.cs
@@ -1,25 +1,24 @@
 using Microsoft.AspNetCore.Diagnostics;
-using Npgsql.Replication;
 
 using server.Domains;
 
 namespace server.Utils.Error
 {
-	public class GlobalExceptionFilter : IExceptionHandler
-	{
-		public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
-		{
-			Response response = new()
-			{
-				Success = false,
-				Error = new Domains.Error()
-				{
-					message = exception.Message,
-				},
-			};
+  public class GlobalExceptionFilter : IExceptionHandler
+  {
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+      Response response = new()
+      {
+        Success = false,
+        Error = new Domains.Error()
+        {
+          Message = exception.Message,
+        },
+      };
 
-			await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
-			return true;
-		}
-	}
+      await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+      return true;
+    }
+  }
 }
