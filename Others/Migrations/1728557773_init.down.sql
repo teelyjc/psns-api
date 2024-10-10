5ee2d0085c35df79790208dc0e929a3dce2c230c
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-10 17:59:34 +0700

    feat: split migration files to `up` and `down`

---
+++ Others/Migrations/1728557773_init.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE users;
+
+DROP TABLE pets;
+
+DROP TABLE sessions;
---
+++ Others/Migrations/1728557773_init.up.sql
@@ -0,0 +1,33 @@
+CREATE TABLE
+  users (
+    id VARCHAR(80) PRIMARY KEY UNIQUE,
+    username VARCHAR(100),
+    password VARCHAR(255),
+    firstname VARCHAR(100),
+    lastname VARCHAR(100),
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+  );
+
+CREATE TABLE
+  pets (
+    id VARCHAR(100) PRIMARY KEY UNIQUE,
+    user_id VARCHAR(100),
+    name VARCHAR(100),
+    type VARCHAR(100),
+    gene VARCHAR(100),
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users (id)
+  );
+
+CREATE TABLE
+  sessions (
+    id VARCHAR(100) PRIMARY KEY UNIQUE,
+    user_id VARCHAR(100),
+    ip_address VARCHAR(100),
+    user_agent VARCHAR(100),
+    created_at TIMESTAMPTZ,
+    expired_at TIMESTAMPTZ,
+    CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users (id)
+  );
