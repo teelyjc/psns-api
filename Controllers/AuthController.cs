7e789797f1b54fc0fda723b7d668022103b3cf9f
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-10 18:06:53 +0700

    feat: add routes for authentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -8,5 +8,17 @@ namespace server.Controllers
 	public class AuthController(AuthUsecases authUsecases) : Controller
 	{
 		private readonly AuthUsecases authUsecases = authUsecases;
+
+		[HttpPost]
+		[Route("signin")]
+		public void SignIn() { }
+
+		[HttpDelete]
+		[Route("signout")]
+		public void SignOut() { }
+
+		[HttpGet]
+		[Route("me")]
+		public void Authenticate() { }
 	}
 }
