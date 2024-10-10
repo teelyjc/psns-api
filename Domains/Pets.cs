f507668743d90c71a416fd062b82e8e1c144adf5
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-10 17:49:29 +0700

    refactor: delete spaces for each fields to same liine and scaffold interfaces for repository and usecase

--- Domains/Pets.cs
+++ Domains/Pets.cs
@@ -2,25 +2,13 @@ namespace server.Domains
 {
   public class Pet
   {
-    public string Id
-    {
-      get;
-      set;
-    } = string.Empty;
-
-    public string UserId
-    {
-      get;
-      set;
-    } = string.Empty;
-
-    public string Type
-    {
-      get;
-      set;
-    } = string.Empty;
-
+    public string Id { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
     public DateTime CreatedAt;
     public DateTime UpdatedAt;
   }
+
+  public interface IPetRepository { }
+  public interface IPetUsecases { }
 }
