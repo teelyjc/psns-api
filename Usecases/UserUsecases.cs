a8bf649ab35c4557f3d2e2c147165fe52ae93d71
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-10 18:16:42 +0700

    feat: remove password before reply and handle `not found`

--- Usecases/UserUsecases.cs
+++ Usecases/UserUsecases.cs
@@ -4,60 +4,62 @@ using server.Domains;
 
 namespace server.Usecases
 {
-  public class UserUsecases(IUserRepository userUsecases) : IUserUsecases
-  {
-    private readonly IUserRepository userUsecases = userUsecases;
+	public class UserUsecases(IUserRepository userUsecases) : IUserUsecases
+	{
+		private readonly IUserRepository userUsecases = userUsecases;
 
-    public async Task<User> GetUserByUsername(string username)
-    {
-      User? user = await this.userUsecases.GetUserByUsername(username);
-      user.Password = "";
+		public async Task<User> GetUserByUsername(string username)
+		{
+			User user = await this.userUsecases.GetUserByUsername(username)
+				?? throw new HttpRequestException("user was not found with this username", null, HttpStatusCode.NotFound);
 
-      return user ?? throw new HttpRequestException("user was not found with this username", null, HttpStatusCode.NotFound);
-    }
+			user.Password = "";
+			return user;
+		}
 
-    public async Task<User> GetUserById(string id)
-    {
-      User? user = await this.userUsecases.GetUserById(id);
-      user.Password = "";
+		public async Task<User> GetUserById(string id)
+		{
+			User? user = await this.userUsecases.GetUserById(id)
+				?? throw new HttpRequestException("user was not found with this id", null, HttpStatusCode.NotFound);
 
-      return user ?? throw new HttpRequestException("user was not found with this id", null, HttpStatusCode.NotFound);
-    }
+			user.Password = "";
+			return user;
+		}
 
-    public async Task SignUpUser(SignUpUser signUp)
-    {
-      DateTime timestamp = DateTime.Now;
+		public async Task SignUpUser(SignUpUser signUp)
+		{
+			DateTime timestamp = DateTime.Now;
 
-      User? user = await this.GetUserByUsername(signUp.Username.ToString());
-      if (user != null)
-      {
-        throw new HttpRequestException("username is already in use", null, HttpStatusCode.Conflict);
-      }
+			User? user = await this.GetUserByUsername(signUp.Username.ToString());
+			if (user != null)
+			{
+				throw new HttpRequestException("username is already in use", null, HttpStatusCode.Conflict);
+			}
 
-      await this.userUsecases.CreateUser(
-        new()
-        {
-          Id = Guid.NewGuid().ToString(),
-          Username = signUp.Username,
-          Password = BCrypt.Net.BCrypt.EnhancedHashPassword(signUp.Password, 10),
-          CreatedAt = timestamp,
-          UpdatedAt = timestamp,
-        }
-      );
-    }
+			await this.userUsecases.CreateUser(
+				new()
+				{
+					Id = Guid.NewGuid().ToString(),
+					Username = signUp.Username,
+					Password = BCrypt.Net.BCrypt.EnhancedHashPassword(signUp.Password, 10),
+					CreatedAt = timestamp,
+					UpdatedAt = timestamp,
+				}
+			);
+		}
 
-    public async Task UpdateUser(UpdateUser updateUser)
-    {
-      User user = await this.GetUserById(updateUser.Id);
+		public async Task UpdateUser(UpdateUser updateUser)
+		{
+			User user = await this.GetUserById(updateUser.Id);
 
-      await this.userUsecases.UpdateUserInfo(
-        new()
-        {
-          Id = user.Id,
-          Firstname = updateUser.Firstname,
-          Lastname = updateUser.Lastname
-        }
-      );
-    }
-  }
+			await this.userUsecases.UpdateUserInfo(
+				new()
+				{
+					Id = user.Id,
+					Firstname = updateUser.Firstname,
+					Lastname = updateUser.Lastname
+				}
+			);
+		}
+	}
 }
