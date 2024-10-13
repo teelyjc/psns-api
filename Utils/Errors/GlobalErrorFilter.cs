14f8160d07b36a2d3e5e1749671811a602ea9e10
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-13 16:34:24 +0700

    feat: check status from httpResquestException

--- Utils/Errors/GlobalErrorFilter.cs
+++ Utils/Errors/GlobalErrorFilter.cs
@@ -8,17 +8,28 @@ namespace server.Utils.Error
   {
     public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
-      Response response = new()
+      if (exception is HttpRequestException e)
       {
-        Success = false,
-        Error = new Domains.Error()
-        {
-          Message = exception.Message,
-        },
-      };
+        httpContext.Response.ContentType = "application/json";
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
-      await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
-      return true;
+        if (e.StatusCode.HasValue)
+        {
+          httpContext.Response.StatusCode = (int)e.StatusCode;
+        }
+
+        await httpContext.Response.WriteAsJsonAsync(new Response()
+        {
+          Success = false,
+          Error = new()
+          {
+            Message = e.Message
+          }
+        }, cancellationToken: cancellationToken);
+        return true;
+      }
+
+      return false;
     }
   }
 }
