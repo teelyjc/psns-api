6dbc81a2a82a27e8e7df7c93abb65c88013a061c
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-13 16:39:13 +0700

    feat: add enum for position and get from context

--- Domains/Users.cs
+++ Domains/Users.cs
@@ -1,14 +1,50 @@
+using System.Net;
+using Npgsql.Replication;
+using NpgsqlTypes;
+
 namespace server.Domains
 {
   public class User
   {
+    public static User GetUserFromContext(HttpContext context)
+    {
+      if (context.Items.TryGetValue("user", out var incomingUser))
+      {
+        if (incomingUser is not User user)
+        {
+          throw new HttpRequestException("user was not found in context", null, HttpStatusCode.InternalServerError);
+        }
+
+        return user;
+      }
+
+      throw new HttpRequestException("user was not set to context", null, HttpStatusCode.InternalServerError);
+    }
+    public static void SetUserToContext(HttpContext context, User user)
+    {
+      context.Items["user"] = user;
+    }
     public string Id { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public string Firstname { get; set; } = string.Empty;
     public string Lastname { get; set; } = string.Empty;
+    [PgName("position")]
+    public Position Position { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+  }
+
+  public enum Position
+  {
+    [PgName("CUSTOMER")]
+    CUSTOMER,
+    [PgName("VETERINARIAN")]
+    VETERINARIAN,
+    [PgName("EXECUTIVE")]
+    EXECUTIVE,
+    [PgName("ADMINISTRATOR")]
+    ADMINISTRATOR
   }
 
   public interface IUserRepository
@@ -16,14 +52,16 @@ namespace server.Domains
     Task<User> GetUserById(string id);
     Task<User> GetUserByUsername(string username);
     Task CreateUser(User user);
-    Task UpdateUserInfo(UpdateUser user);
+    Task UpdateUserById(string userId, UserUpdateInputs user);
+    Task<User> GetUserBySessionId(string id);
   }
   public interface IUserUsecases
   {
     Task<User> GetUserByUsername(string username);
     Task<User> GetUserById(string id);
-    Task SignUpUser(SignUpUser user);
-    Task UpdateUser(UpdateUser user);
+    Task SignUp(SignUpUser user);
+    Task UpdateUserById(string userId, UserUpdateInputs user);
+    Task<User> GetUserBySessionId(string id);
   }
 
   public class SignUpUser
@@ -35,9 +73,8 @@ namespace server.Domains
     public string Lastname { get; set; } = string.Empty;
   }
 
-  public class UpdateUser
+  public class UserUpdateInputs
   {
-    public string Id { get; set; } = string.Empty;
     public string Firstname { get; set; } = string.Empty;
     public string Lastname { get; set; } = string.Empty;
   }
