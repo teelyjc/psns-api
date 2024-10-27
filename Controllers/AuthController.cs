6c8028b76d9ae32acbe4d374032357c75ddc55c7
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-27 22:08:07 +0700

    feat: use sub routes for auth

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -6,63 +6,63 @@ using server.Middlewares;
 
 namespace server.Controllers
 {
-	[ApiController]
-	[Route("auth")]
-	public class AuthController(IAuthUsecases authUsecases) : Controller
-	{
-		private readonly IAuthUsecases authUsecases = authUsecases;
+  [ApiController]
+  [Route("auth")]
+  public class AuthController(IAuthUsecases authUsecases) : Controller
+  {
+    private readonly IAuthUsecases authUsecases = authUsecases;
 
-		[HttpPost("/signin")]
-		public async Task<ActionResult<Response>> SignIn(SignInCredentials credentials)
-		{
-			IPAddress? ip = this.HttpContext.Connection.RemoteIpAddress;
-			string userAgent = this.Request.Headers.UserAgent.ToString();
+    [HttpPost("signin")]
+    public async Task<ActionResult<Response>> SignIn(SignInCredentials credentials)
+    {
+      IPAddress? ip = this.HttpContext.Connection.RemoteIpAddress;
+      string userAgent = this.Request.Headers.UserAgent.ToString();
 
-			var header = await this.authUsecases.SignIn(new()
-			{
-				IpAddress = ip?.ToString() ?? "",
-				UserAgent = userAgent,
-				Username = credentials.Username,
-				Password = credentials.Password
-			});
+      var header = await this.authUsecases.SignIn(new()
+      {
+        IpAddress = ip?.ToString() ?? "",
+        UserAgent = userAgent,
+        Username = credentials.Username,
+        Password = credentials.Password
+      });
 
-			this.Response.Cookies.Append("sid", header.CookieHeader, header.CookieOptions);
+      this.Response.Cookies.Append("sid", header.CookieHeader, header.CookieOptions);
 
-			return new Response()
-			{
-				Success = true,
-				Data = new Success() { Message = "signed in successfully" }
-			};
-		}
+      return new Response()
+      {
+        Success = true,
+        Data = new Success() { Message = "signed in successfully" }
+      };
+    }
 
-		[HttpDelete("/signout")]
-		[ServiceFilter(typeof(AuthMiddleware))]
-		public async Task<ActionResult<Response>> SignOut()
-		{
-			var header = await this.authUsecases.SignOut(new()
-			{
-				CookieHeader = this.HttpContext.Request.Cookies["sid"]!
-			});
+    [HttpDelete("signout")]
+    [ServiceFilter(typeof(AuthMiddleware))]
+    public async Task<ActionResult<Response>> SignOut()
+    {
+      var header = await this.authUsecases.SignOut(new()
+      {
+        CookieHeader = this.HttpContext.Request.Cookies["sid"]!
+      });
 
-			this.Response.Cookies.Append("sid", header.CookieHeader, header.CookieOptions);
+      this.Response.Cookies.Append("sid", header.CookieHeader, header.CookieOptions);
 
-			return new Response()
-			{
-				Success = true,
-				Data = new Success() { Message = "signed out successfully" }
-			};
-		}
+      return new Response()
+      {
+        Success = true,
+        Data = new Success() { Message = "signed out successfully" }
+      };
+    }
 
-		[HttpGet("me")]
-		[ServiceFilter(typeof(AuthMiddleware))]
-		public ActionResult<Response> Me()
-		{
-			User user = Domains.User.GetUserFromContext(this.HttpContext);
-			return new Response()
-			{
-				Success = true,
-				Data = user
-			};
-		}
-	}
+    [HttpGet("me")]
+    [ServiceFilter(typeof(AuthMiddleware))]
+    public ActionResult<Response> Me()
+    {
+      User user = Domains.User.GetUserFromContext(this.HttpContext);
+      return new Response()
+      {
+        Success = true,
+        Data = user
+      };
+    }
+  }
 }
